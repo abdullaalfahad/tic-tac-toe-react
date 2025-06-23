1fa81f14e5eac86c24336f86b29469567105eb4a
fahadevidentbd <fahad@evidentbd.com>
2025-06-23 14:53:36 +0600

    feat: calculate winner and show status

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,16 @@ function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState(true);
 
+  const winner = calculateWinner(squares);
+
+  let status;
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Nextplayer: ${isXNext ? "X" : "O"}`;
+  }
+
   const handleClick = (i) => {
     if (squares[i]) return;
 
@@ -22,7 +32,9 @@ function Board() {
   };
 
   return (
-    <div>
+    <div className="m-10">
+      <h2 className="text-2xl mb-4">{status}</h2>
+
       <div className="flex">
         <Square value={squares[0]} handleClick={() => handleClick(0)} />
         <Square value={squares[1]} handleClick={() => handleClick(1)} />
@@ -40,6 +52,26 @@ function Board() {
       </div>
     </div>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board;
